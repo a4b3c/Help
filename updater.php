4beef43e192c31ed6b9bac46d3a00cd086cfcad2
a4b3c <164261719+a4b3c@users.noreply.github.com>
2024-03-21 04:16:03 +0330

    Update updater.php

--- updater.php
+++ updater.php
@@ -1,7 +1,24 @@
 <?php
-// Fetch data from https://c26.sub-v2.workers.dev/frag and save it to custom.txt
-file_put_contents('custom.txt', file_get_contents('https://c26.sub-v2.workers.dev/frag'));
+// Enable error reporting
+ini_set("display_errors", 1);
+ini_set("display_startup_errors", 1);
+error_reporting(E_ERROR | E_PARSE);
 
-// Fetch data from https://c26.sub-v2.workers.dev/sub and save it to normal.txt
-file_put_contents('normal.txt', file_get_contents('https://c26.sub-v2.workers.dev/sub'));
+// Function to fetch data from URL and save it to a file
+function fetchDataAndSave($url, $filename)
+{
+    $data = file_get_contents($url);
+    if ($data !== false) {
+        file_put_contents($filename, $data);
+        echo "Data fetched from $url and saved to $filename\n";
+    } else {
+        echo "Failed to fetch data from $url\n";
+    }
+}
+
+// Fetch and save data from https://c26.sub-v2.workers.dev/frag to custom.txt
+fetchDataAndSave('https://c26.sub-v2.workers.dev/frag', 'custom.txt');
+
+// Fetch and save data from https://c26.sub-v2.workers.dev/sub to normal.txt
+fetchDataAndSave('https://c26.sub-v2.workers.dev/sub', 'normal.txt');
 ?>
